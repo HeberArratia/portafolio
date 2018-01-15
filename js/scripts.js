8ae16905293c75505b39ab54ecea16cd09ada2e5
Heber Arratia <heber.im87@gmail.com>
2018-01-15 00:48:00 -0300

    se mejora responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
 
-	$('.product-gallery').flickity({
-		  // options
-		autoPlay: 3000
-	});
-
-	$('.go-products').navScroll();
-
-	$(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
-    });
+	
     
 });
